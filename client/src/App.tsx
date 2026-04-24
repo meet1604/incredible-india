e5ad20980d4a5e7705695cb96f7325ac60aca7ac
Meet Parmar <design.team1@lwt.app>
2026-04-24 15:39:37 +0530

    Temporarily comment out globe loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Switch, Route, useLocation } from "wouter";
+// import { useState } from "react";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,7 +8,7 @@ import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import Cookbook from "@/pages/cookbook";
 import Admin from "@/pages/admin";
-import { GlobeLoader } from "@/components/GlobeLoader";
+// import { GlobeLoader } from "@/components/GlobeLoader";
 
 function Router() {
   return (
@@ -21,25 +21,25 @@ function Router() {
   );
 }
 
-function AppContent() {
-  const [location] = useLocation();
-  const [loaded, setLoaded] = useState(false);
-  const showLoader = !loaded && location === "/";
-
-  return (
-    <>
-      {showLoader && <GlobeLoader onComplete={() => setLoaded(true)} />}
-      <Router />
-    </>
-  );
-}
+// function AppContent() {
+//   const [location] = useLocation();
+//   const [loaded, setLoaded] = useState(false);
+//   const showLoader = !loaded && location === "/";
+//
+//   return (
+//     <>
+//       {showLoader && <GlobeLoader onComplete={() => setLoaded(true)} />}
+//       <Router />
+//     </>
+//   );
+// }
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <AppContent />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
