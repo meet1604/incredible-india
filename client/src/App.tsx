fd31f1d3d5c3345469584ff7ca6e8d8e188f12c5
Meet Parmar <design.team1@lwt.app>
2026-04-24 15:41:09 +0530

    Remove globe loader and related packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -8,7 +7,6 @@ import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import Cookbook from "@/pages/cookbook";
 import Admin from "@/pages/admin";
-// import { GlobeLoader } from "@/components/GlobeLoader";
 
 function Router() {
   return (
@@ -20,19 +18,6 @@ function Router() {
     </Switch>
   );
 }
-
-// function AppContent() {
-//   const [location] = useLocation();
-//   const [loaded, setLoaded] = useState(false);
-//   const showLoader = !loaded && location === "/";
-//
-//   return (
-//     <>
-//       {showLoader && <GlobeLoader onComplete={() => setLoaded(true)} />}
-//       <Router />
-//     </>
-//   );
-// }
 
 function App() {
   return (
